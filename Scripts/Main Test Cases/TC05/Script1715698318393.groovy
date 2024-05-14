1e9a328878b40ed22d9226a72d8b698fa8484969
linhnty2@gmail.com <nty.linh0326@gmail.com>
2024-05-14 22:21:34 +0700

    add checkpoint

---
+++ Scripts/Main Test Cases/TC05/Script1715698318393.groovy
@@ -0,0 +1,79 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+
+WebUI.rightClick(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'), 0)
+
+WebUI.verifyCheckpoint(findCheckpoint('Checkpoints/CP01'), false)
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/div_Demo account_col-sm-offset-4 col-sm-8'))
+
+WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 'John Doe')
+
+WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'ThisIsNotAPassword')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/div_Login                Please login to ma_5151a8'))
+
+WebUI.rightClick(findTestObject('Object Repository/Page_CURA Healthcare Service/div_Login                Please login to ma_5151a8'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/label_Username'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/label_Password'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/label_Username'))
+
+WebUI.rightClick(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/section_Login                Please login t_80017f'))
+
+WebUI.rightClick(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Login'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Login'))
+
+WebUI.takeElementScreenshot(findTestObject('Page_CuraAppointment/chk_Medicaid'))
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.takeElementScreenshot(findTestObject('Page_CuraAppointment/div_Appointment'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/section_Make Appointment                   _57f49e'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/div_Demo account_col-sm-offset-4 col-sm-8'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/span_Visit Date (Required)_glyphicon glyphi_cada34'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/td_22'))
+
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Book Appointment'))
+
+WebUI.takeScreenshot()
+
+WebUI.takeFullPageScreenshot()
+
+WebUI.closeBrowser()
+
